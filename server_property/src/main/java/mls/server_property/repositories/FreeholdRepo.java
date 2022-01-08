bb3a8d31bc73138cd066629d92834c8c8d399cb1
sss <10131931+sowiesss@users.noreply.github.com>
2022-01-07 22:15:37 -0500

    commit

--- server_property/src/main/java/mls/server_property/repositories/FreeholdRepo.java
+++ server_property/src/main/java/mls/server_property/repositories/FreeholdRepo.java
@@ -1,7 +1,14 @@
 package mls.server_property.repositories;
 
+import mls.server_property.domain.Freehold;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import java.util.List;
+import java.util.Optional;
+
 @NoRepositoryBean
-public interface FreeholdRepo extends ResidentialRepo{
+public interface FreeHoldRepo extends ResidentialRepo{
+    Optional<List<Freehold>> findFreeholdByType(String type);
+    Optional<List<Freehold>> findFreeholdByMultiGenerationType(boolean isMultigeneration);
+    Optional<List<Freehold>> findFreeholdByNumberOfFloors(int numberOfFloors);
 }
